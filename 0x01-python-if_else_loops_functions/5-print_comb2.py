de62dfd5d64612a81b1cdc8fac90440343e40a56
njdam <jeandamngaboyimana@gmail.com>
2023-06-06 22:45:16 +0000

    A program that prints numbers from 0 to 99.

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# printing number from 0 to 99 separated by comma and space.
+for num in range(100):
+    if num == 99:
+        print("{}".format(num))
+    else:
+        print("{:02}, ".format(num), end="")
