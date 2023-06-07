c7f77bf691aacd2eb18e01401e0def58402b7b99
njdam <jeandamngaboyimana@gmail.com>
2023-06-07 03:00:47 +0000

    Python function def magic_calculation(a, b, c): that does exactly the same as the Python bytecode.

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# To match with bytecode provide
+
+def magic_calculation(a, b, c):
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    else:
+        return (a * b - c)
