af3dcaaecb63e41cfe72e4341bf240bdf11b7e4a
njdam <jeandamngaboyimana@gmail.com>
2023-06-08 16:12:11 +0000

    A program that print a value of imported variable a from .py file.

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # A script to print imported variable a
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # code should not be executed when imported
     from variable_load_5 import a
     print("{}".format(a))
