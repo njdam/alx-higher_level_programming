d0e610428b3a17a8861a2ea60847ef1faac44ebd
njdam <jeandamngaboyimana@gmail.com>
2023-06-11 22:06:05 +0000

    A function to find and return maximum integer in

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/pyhton3
-# A function to find and return a maximum integer in a list.
+# A function to find and return a largest integer in a list.
 
 
 def max_integer(my_list=[]):
@@ -9,12 +9,12 @@ def max_integer(my_list=[]):
         return ("None")
 
     else:
-        maxim = 0
+        num = 0
         for i in range(lens - 1):
-            if my_list[i] < my_list[i + 1] and my_list[i + 1] > maxim:
-                maxim = my_list[i + 1]
+            if my_list[i] < my_list[i + 1] and my_list[i + 1] > num:
+                num = my_list[i + 1]
 
-            elif my_list[i] > maxim:
-                maxim = my_list[i]
+            elif my_list[i] > num:
+                num = my_list[i]
 
-        return (maxim)
+        return (num)
