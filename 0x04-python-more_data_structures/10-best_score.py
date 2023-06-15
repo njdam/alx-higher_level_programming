eb7003962770462c0a9bd08097f13aeea5380eb3
njdam <jeandamngaboyimana@gmail.com>
2023-06-15 00:50:09 +0000

    A function to return a key with a best score in a dictionary.

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# A function to return a key with a best score in a dictionary.
+
+
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return (None)
+
+    best_key = max(a_dictionary, key=a_dictionary.get)
+
+    return (best_key)
