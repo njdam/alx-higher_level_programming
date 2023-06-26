33d3dac130fee24e872ee53633e7c7365c84cc9e
njdam <jeandamngaboyimana@gmail.com>
2023-06-26 23:49:21 +0000

    A function to return result by a function to two args.

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,10 +4,12 @@
 
 def safe_function(fct, *args):
     import sys
+    result = None
+
     try:
         result = fct(*args)
         return (result)
 
-    except:
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (None)
+    except Exception as AnyErr:
+        print("Exception: {}".format(AnyErr), file=sys.stderr)
+        return (result)
