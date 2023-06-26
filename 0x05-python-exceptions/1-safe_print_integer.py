f053809312bd7683adbda67170a27958bdf3440c
njdam <jeandamngaboyimana@gmail.com>
2023-06-26 17:02:49 +0000

    A function to print an integer otherwise exception error.

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# A function to an integer otherwise exception error
+
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+    except ValueError:
+        return (False)
+    else:
+        return (True)
