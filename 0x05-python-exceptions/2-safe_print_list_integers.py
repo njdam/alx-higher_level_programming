04622a8566c355ea252d71365fdfe8fae62ce1f1
njdam <jeandamngaboyimana@gmail.com>
2023-06-26 17:25:34 +0000

    A function to print all integers in x elements of my_list.

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# A function to print integers in x elements from my_list elements
+
+def safe_print_list_integers(my_list=[], x=0):
+    rval = 0
+    try:
+        for i in range(x):
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
+                rval += 1
+    except IndexError:
+        pass
+    print("")
+    return (rval)
