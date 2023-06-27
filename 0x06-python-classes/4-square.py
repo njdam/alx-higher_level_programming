55c18863b602d5a3d2f3962f461aaec6aac5a8bf
njdam <jeandamngaboyimana@gmail.com>
2023-06-27 19:18:00 +0000

    A class Square that define a square and get and setting size to new value.

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""A class Square to define a square with private size and raise Error"""
+
+
+class Square:
+    """Define a square"""
+
+    def __init__(self, size=0):
+        """
+        Initialisation of new square size;
+
+        Args:
+            size (int): is size of new square must be an integer not < 0.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Getting current value of size of a square"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """
+        If size is not an integer to raise TypeError with a message;
+        If is Less than Zero to raise ValueError with a message;
+        setting current value to size of a square;
+
+        Args:
+            value (int): is size of new square must be an integer not < 0.
+        """
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """A puplic instance methode to return current square area"""
+        return (self.__size ** 2)
