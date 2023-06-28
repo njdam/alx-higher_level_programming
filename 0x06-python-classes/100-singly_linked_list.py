cec5af6ff99741b4f87061034653d55e0e9fddc8
njdam <jeandamngaboyimana@gmail.com>
2023-06-28 06:21:26 +0000

    A creation of Python singly linked list by OOP.

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,18 +1,31 @@
 #!/usr/bin/python3
+"""A class of Node and class of SinglyLinkedList
+for dealing with Python Singly linked list.
+"""
 
 
 class Node:
+    """A class Node for defining a node of singly linked list."""
 
     def __init__(self, data, next_node=None):
+        """
+        Initialisation of Node to data and new node
+
+        Args:
+            data (int): is a integer data to be stored in current node
+            next_node (Node): is the next node to new node.
+        """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
+        """Getting current data at current Node."""
         return (self.__data)
 
     @data.setter
     def data(self, value):
+        """Setting new value to data of current Node."""
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
 
@@ -20,6 +33,7 @@ class Node:
 
     @property
     def next_node(self):
+        """Getting next Node to new Node or current Node."""
         return (self.__next_node)
 
     @next_node.setter
@@ -31,36 +45,39 @@ class Node:
 
 
 class SinglyLinkedList:
+    """A class SinglyLinkedList for Python singly linked list."""
 
     def __init__(self):
         """Initalisation of a SinglyLinkedList."""
         self.__head = None
 
     def sorted_insert(self, value):
-        new = Node(value)
+        """A public instance to insert new node in increasing order."""
+        new_node = Node(value)
         if self.__head is None:
-            new.next_node = None
-            self.__head = new
+            new_node.next_node = None
+            self.__head = new_node
 
         elif self.__head.data > value:
-            new.next_node = self.__head
-            self.__head = new
+            new_node.next_node = self.__head
+            self.__head = new_node
 
         else:
-            tmp = self.__head
-            while (tmp.next_node is not None and
-                    tmp.next_node.data < value):
-                tmp = tmp.next_node
+            temp = self.__head
+            while ((temp.next_node is not None) and
+                    (temp.next_node.data < value)):
+                temp = temp.next_node
 
-            new.next_node = tmp.next_node
-            tmp.next_node = new
+            new_node.next_node = temp.next_node
+            temp.next_node = new_node
 
     def __str__(self):
-        values = []
-        tmp = self.__head
+        """Return string that provide formated representation."""
+        svalue = []
+        temp = self.__head
 
-        while tmp is not None:
-            values.append(str(tmp.data))
-            tmp = tmp.next_node
+        while temp is not None:
+            svalue.append(str(temp.data))
+            temp = temp.next_node
 
-        return ('\n'.join(values))
+        return ('\n'.join(svalue))
