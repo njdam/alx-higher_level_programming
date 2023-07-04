94e7f4a93bef74a7bbd3ed4ef4ff13c8bdf28194
njdam <jeandamngaboyimana@gmail.com>
2023-07-04 15:19:59 +0000

    A function to print a text with 2 new lines after `.`, `?`, and `:` end=

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""A file with a function that prints a text with 2 new lines
+after each of these characters: ., ? and :
+"""
+
+
+def text_indentation(text):
+    """A function that prints a text with 2 new lines
+    after each of these characters: '.', '?' and ':'
+    and no spaces at end or start of each printed line
+
+    Parameter:
+        text (str): is text indenation and must be a string
+
+    Raise:
+        TypeError: if text is not a string.
+    """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    punct = [".", "?", ":"]
+    line = ""
+
+    for char in text:
+        line += char
+        if char in punct:
+            line = line.strip()  # to remove trailing white space
+            if line.startswith(" "):  # remove spaces where line starts
+                line = line[1:]
+            print("{}\n".format(line))
+            line = ""  # After printing line start with empty new line
+
+    if line.strip():  # if last line is not empty after removing tr..sapces
+        if line.startswith(" "):  # remove spaces where last line started
+            line = line[1:]
+        print("{}".format(line.strip()), end="")
