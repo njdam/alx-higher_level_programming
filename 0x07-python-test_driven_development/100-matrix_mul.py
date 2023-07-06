757f943521ce473fded54cb1c5c29e49a7739e88
njdam <jeandamngaboyimana@gmail.com>
2023-07-06 19:47:04 +0000

    A function to multiply 2 matrices.

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,41 +1,46 @@
 #!/usr/bin/python3
+"""Defines a matrix multiplication function."""
 
 
 def matrix_mul(m_a, m_b):
-    # If matrix is empty
+    """Multiply two matrices.
+
+    Args:
+        m_a (list of lists of ints/floats): The first matrix.
+        m_b (list of lists of ints/floats): The second matrix.
+    Raises:
+        TypeError: If either m_a or m_b is not a list of lists of ints/floats.
+        TypeError: If either m_a or m_b is empty.
+        TypeError: If either m_a or m_b has different-sized rows.
+        ValueError: If m_a and m_b cannot be multiplied.
+    Returns:
+        A new matrix representing the multiplication of m_a by m_b.
+    """
+
     if m_a == [] or m_a == [[]]:
         raise ValueError("m_a can't be empty")
-
     if m_b == [] or m_b == [[]]:
         raise ValueError("m_b can't be empty")
 
-    # If matrix is not a list
     if not isinstance(m_a, list):
         raise TypeError("m_a must be a list")
-
     if not isinstance(m_b, list):
         raise TypeError("m_b must be a list")
 
-    # If matrix is not a list of lists
     if not all(isinstance(row, list) for row in m_a):
         raise TypeError("m_a must be a list of lists")
-
     if not all(isinstance(row, list) for row in m_b):
         raise TypeError("m_b must be a list of lists")
 
-    # If matrix is not contain elements wich is either integer or float
-    if not all((isinstance(num, int) or isinstance(num, float))
-               for num in [column for row in m_a for column in row]):
+    if not all((isinstance(col, int) or isinstance(col, float))
+               for col in [num for row in m_a for num in row]):
         raise TypeError("m_a should contain only integers or floats")
-
-    if not all((isinstance(num, int) or isinstance(num, float))
-               for num in [column for row in m_b for column in row]):
+    if not all((isinstance(col, int) or isinstance(col, float))
+               for col in [num for row in m_b for num in row]):
         raise TypeError("m_b should contain only integers or floats")
 
-    # If matrix contain unequally rows
     if not all(len(row) == len(m_a[0]) for row in m_a):
         raise TypeError("each row of m_a must should be of the same size")
-
     if not all(len(row) == len(m_b[0]) for row in m_b):
         raise TypeError("each row of m_b must should be of the same size")
 
