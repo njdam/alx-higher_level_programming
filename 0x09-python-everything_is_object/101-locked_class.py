1026efcaf2b0f32a1e2559386d4b40f3d1bac165
njdam <jeandamngaboyimana@gmail.com>
2023-07-06 12:29:51 +0000

    A class LockedClass with no class or object attribute and prevent the user to create new one.

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A file for a locked class."""
+
+
+class LockedClass:
+    """A call prevent the user from creating new instance atrributes."""
+
+    __slots__ = "first_name"
