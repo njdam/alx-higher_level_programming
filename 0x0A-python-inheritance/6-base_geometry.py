e7f68b6fcc93224c646ff391b84143c53a0b8725
njdam <jeandamngaboyimana@gmail.com>
2023-07-12 15:20:17 +0000

    A class BaseGeometry (based on 5-base_geometry.py). to  Improve Geometry.

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A file with empty class called BaseGeometry."""
+
+
+class BaseGeometry:
+    """An empty class called BaseGeometry."""
+
+    def area(self):
+        raise Exception("area() is not implemented")
