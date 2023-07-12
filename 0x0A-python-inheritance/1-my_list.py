8c1e5cefb4d165f3fc5a5580dbb308e69b303b47
njdam <jeandamngaboyimana@gmail.com>
2023-07-12 13:57:17 +0000

    A class with a function to print sorted list in ascending order.

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A file with a class MyList that inherits from list."""
+
+
+class MyList(list):
+    """A class MyList that inherits from list"""
+
+    def print_sorted(self):
+        """A function to print sorted element of a list"""
+        
+        print(sorted(self))
