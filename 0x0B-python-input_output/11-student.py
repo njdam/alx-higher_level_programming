215b92d970ec31362cd6ac2322951b834bafe2a9
njdam <jeandamngaboyimana@gmail.com>
2023-07-13 11:41:01 -0700

    A class Student to define a student by: (based on 10-student.py).

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""A file defines a class Student which define a student."""
+
+
+class Student:
+    """A class Student which define a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialisation of class Student with first and last name and age
+
+        Args:
+            first_name (str): is a first name of a student.
+            last_name (str): is a last name of a student.
+            age (int): is an age of a student.
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """A function to get dictionary represantation of a student
+
+        Args:
+            attrs (list): is a list of a strings.
+        """
+
+        if (type(attrs) == list and
+                all((type(item) == str) for item in attrs)):
+            return {s: getattr(self, s) for s in attrs if hasattr(self, s)}
+
+        return (self.__dict__)
+
+    def reload_from_json(self, json):
+        """A function to replace all attributes of the Student instance
+
+        Args:
+            json (dict): will be always a dictionary.
+        """
+        
+        for s, item in json.items():
+            setattr(self, s, item)
