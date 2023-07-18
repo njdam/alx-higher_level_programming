edc4928415715665dcf6195a61da715dc43e67eb
njdam <jeandamngaboyimana@gmail.com>
2023-07-18 13:55:03 +0000

    Update the class Rectangle by overriding the __str__ method.

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python3
+"""A file bearing a class Rectangle which inherited from Base."""
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """A Rectangle class which inherited from Base class."""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialisation of Rectangle class with #nt args
+
+        Args:
+            width (int): is width of rectangle must be an integer
+            height (int): is height of rectangle must be an integer
+            x (int): is value of x must be an int
+            y (int): is value of y must be an int
+            id (int): is value of id must not be a None.
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    # Getting and Setting value of width
+    @property
+    def width(self):
+        """Retrieving a value of width."""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """Setting width to a value."""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
+        self.__width = value
+
+    # Getting and Setting value of height
+    @property
+    def height(self):
+        """Retrieving a value of height."""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """Setting height to a value."""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+
+        self.__height = value
+
+    # Getting and Setting value of x
+    @property
+    def x(self):
+        """Retrieving a value of x."""
+        return (self.__x)
+
+    @x.setter
+    def x(self, value):
+        """Setting x to a value."""
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+
+        self.__x = value
+
+    # Getting and Setting value of y
+    @property
+    def y(self):
+        """Retrieving a value of y."""
+        return (self.__y)
+
+    @y.setter
+    def y(self, value):
+        """Setting y to a value."""
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+
+        self.__y = value
+
+    def area(self):
+        """A function to return area of a rectangle."""
+        return (self.__width * self.__height)
+
+    def display(self):
+        """A function to print area of rectangle by # display."""
+        for y in range(self.__height):
+            for x in range(self.__width):
+                print("#", end="")
+            print("")
+
+    def __str__(self):
+        """A function for returning string representation of rectangle."""
+        string = "[" + str(getattr(Rectangle, '__name__')) + "]"
+        string += " " + str(f"({self.id}) {self.__x}/{self.__y}")
+        string += " " + str(f"- {self.__width}/{self.__height}")
+
+        return (string)
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+"""A Test Case file to test rectangle.py file by unittest Test Cases."""
+
+import unittest
+from unittest.mock import patch
+
+from io import StringIO
+
+from models.base import Base
+from models.rectangle import Rectangle
+
+
+class TestingClassRectangle(unittest.TestCase):
+    """A test cases for testing a rectangle inherited from Base class."""
+
+    def test_rectangle_if_is_base(self):
+        self.assertIsInstance(Rectangle(5, 7), Base)
+
+    def test_width_height_only(self):
+        r1 = Rectangle(10, 2)
+        r2 = Rectangle(2, 10)
+        self.assertEqual(r2.id, r1.id + 1)
+
+    def test_with_all_args(self):
+        r3 = Rectangle(10, 2, 0, 0, 12)
+        self.assertEqual(r3.id, 12)
+
+    def test__type_error(self):
+        with self.assertRaises(TypeError):
+            Rectangle(10, "2")
+            Rectangle("6", 7)
+
+    def test_neg_value(self):
+        with self.assertRaises(ValueError):
+            Rectangle(10, 2, 3, -1)
+            Rectangle(10, 2, {}, 0)
+            Rectangle(10, -2)
+            Rectangle(-10, 2)
+
+    def test_area_rectangle(self):
+        r1 = Rectangle(3, 2)
+        r2 = Rectangle(2, 10)
+        r3 = Rectangle(8, 7, 0, 0, 12)
+        self.assertEqual(r1.area(), 6)
+        self.assertEqual(r2.area(), 20)
+        self.assertEqual(r3.area(), 56)
+
+    def test_display_rectangle(self):
+        r1 = Rectangle(4, 6)
+        expected_output = "####\n####\n####\n####\n####\n####\n"
+        with patch("sys.stdout", new=StringIO()) as output:
+            r1.display()
+            self.assertEqual(output.getvalue(), expected_output)
+
+        r2 = Rectangle(2, 2)
+        expected_output = "##\n##\n"
+        with patch("sys.stdout", new=StringIO()) as output:
+            r2.display()
+            self.assertEqual(output.getvalue(), expected_output)
+
+    def test_str_repr(self):
+        r1 = Rectangle(4, 6, 2, 1, 12)
+        expected_r1 = "[Rectangle] (12) 2/1 - 4/6\n"
+        with patch("sys.stdout", new=StringIO()) as output:
+            print(r1)
+            self.assertEqual(output.getvalue(), expected_r1)
+
+        r2 = Rectangle(5, 5, 1, 0)
+        r3 = Rectangle(5, 5, 1)
+        expected_r3 = f"[Rectangle] ({r2.id + 1}) 1/0 - 5/5\n"
+        with patch("sys.stdout", new=StringIO()) as output:
+            print(r3)
+            self.assertEqual(output.getvalue(), expected_r3)
+
+
+if __name__ == '__main__':
+    unittest.main()
