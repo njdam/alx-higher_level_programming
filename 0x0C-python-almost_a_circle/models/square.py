9cd66e3878471484403a492bfd6a06b70eaccf93
njdam <jeandamngaboyimana@gmail.com>
2023-07-19 09:53:24 +0000

    Updated

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A file bearing class Square inherited from class Rectangle."""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """A class Square inherits Rectangle to deal with a square."""
