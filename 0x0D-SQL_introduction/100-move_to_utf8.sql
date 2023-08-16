668d24015c205723439f107b1c01318a542faabc
njdam <jeandamngaboyimana@gmail.com>
2023-08-16 22:07:38 +0000

    A script that converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci).

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -4,5 +4,6 @@
 -- Database hbtn_0c_0
 -- Table first_table
 -- Field name in first_table
-USE hbtn_0c_0 ALTER TABLE first_table
+USE hbtn_0c_0
+ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
