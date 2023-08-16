4dee35f9a293b9a0db72684ffe2efcbd40959f4e
njdam <jeandamngaboyimana@gmail.com>
2023-08-16 20:57:17 +0000

    A script that displays the number of records with id = 89 in the table first_table.

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -3,12 +3,12 @@
 -- second_table description: id INT, name VARCHAR(256), score INT
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 
--- A script should create these records:
+-- A script should create these 4 records below:
 -- id = 1, name = “John”, score = 10
 -- id = 2, name = “Alex”, score = 3
 -- id = 3, name = “Bob”, score = 14
 -- id = 4, name = “George”, score = 8
-INSERT INTO second_table (id, name, score) VALUES (1, "John", 10)
-INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3)
-INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14)
-INSERT INTO second_table (id, name, score) VALUES (4, "George", 8)
+INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
+INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
+INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
+INSERT INTO second_table (id, name, score) VALUES (4, "George", 8);
