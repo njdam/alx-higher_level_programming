5c75e07310be24f61e80b37dddb2203baa468e41
njdam <jeandamngaboyimana@gmail.com>
2023-09-11 13:07:15 +0000

    A script that prints “JavaScript is amazing”.

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
 /* A script that prints “JavaScript is amazing” */
 
-console.log('JavaScript is amazing');
+// Declaration of constant variable called myVar with string
+const myVar = 'JavaScript is amazing';
+// Printing content of myVar
+console.log(myVar);
