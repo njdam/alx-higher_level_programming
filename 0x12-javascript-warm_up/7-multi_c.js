5b11d049a3467956956df1297289bd2b3a46e27e
njdam <jeandamngaboyimana@gmail.com>
2023-09-11 20:55:21 +0000

    A script that prints x times “C is fun”.

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,15 +2,16 @@
 /* A script that prints x times “C is fun” */
 
 const string = 'C is fun';
-
 const args = process.argv.slice(2);
 
 if (args.length === 0) {
   console.log('Missing number of occurrences');
 } else {
-  const max = process.argv[2];
+  // x is the first argument of the script
+  const x = process.argv[2];
+
   let i = 0;
-  while (i < max) {
+  while (i < x) {
     console.log(string);
     i++;
   }
