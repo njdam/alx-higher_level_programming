5278fd4f4c6ed943dddccf0e5fbe826c61b28d60
njdam <jeandamngaboyimana@gmail.com>
2023-09-12 18:19:57 +0000

    A class Rectangle that defines a rectangle #2.

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/* A class Rectangle that defines a rectangle */
+
+class Rectangle {
+  // A class Rectangle that defines a Rectangle with attributtes
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      // Create empty object if w or h is not a positive integer
+      Object.create(null);
+    }
+  }
+}
+
+module.exports = Rectangle;
