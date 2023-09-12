c0ef572caeb2cb6380c938a1f38c1dea646fc65e
njdam <jeandamngaboyimana@gmail.com>
2023-09-12 02:13:17 +0000

    An Update of script to replace the value of 12 with 89 in myObject.

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/* A script to replace the value 12 with 89 */
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+// Change the value of myObject
+myObject.value = 89;
+
+console.log(myObject);
