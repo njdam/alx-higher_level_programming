10bbcc5acef7d8b1a0e59295dd0956b1d8a181f4
njdam <jeandamngaboyimana@gmail.com>
2023-09-21 11:55:27 +0000

    A  script that prints the State object with the name passed as argument from the database hbtn_0e_6_usa.

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -6,22 +6,24 @@
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sys import argv
-import re
+from sys import argv, exit
 
 
 if __name__ == '__main__':
+    """A script must receive 4 parameters to find state id"""
+    if len(argv) != 5:
+        print(f'Usage: {argv[0]} <user> <passwd> <database> <state_name>')
+        exit(1)
+
     DATABASE_URL = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
     engine = create_engine(DATABASE_URL.format(argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
+    state_name = argv[4]
+    found = session.query(State).filter(State.name == state_name).first()
 
-    for instance in session.query(State).order_by(State.id):
-        escaped_name = re.escape(instance.name)
-        matched = re.match(escaped_name, argv[4])
-
-    if matched is not None:
-        print(matched.id)
+    if found is not None:
+        print(found.id)
     else:
         print("Not found")
