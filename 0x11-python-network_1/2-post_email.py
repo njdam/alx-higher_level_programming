fd5ceaddf6b9a40dba33373390b8d9e5b41da860
njdam <jeandamngaboyimana@gmail.com>
2023-09-29 18:21:51 +0000

    2. POST an email #0

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A Python script that takes in a URL and an email, sends a POST request 
+to the passed URL with the email as a parameter, and displays
+the body of the response (decoded in utf-8)
+"""
+
+from urllib import request, parse
+from sys import argv
+
+
+if __name__ == '__main__':
+    if len(argv) > 2:
+        dec_data = bytes(parse.urlencode([('email', argv[2])]), 'utf-8')
+        with request.urlopen(argv[1], data=dec_data) as response:
+            print(response.read().decode('utf-8'))
