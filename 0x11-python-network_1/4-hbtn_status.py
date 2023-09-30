be141b81129b062979edc72c92afb8826bc11b38
njdam <jeandamngaboyimana@gmail.com>
2023-09-30 04:52:10 +0000

    4. What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,7 +6,7 @@ import requests
 
 
 if __name__ == '__main__':
-    responses = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:")
-    print("\t- type: {}".format(type(responses.text)))
-    print("\t- content: {}".format(responses.text))
+    response = requests.get('https://intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type: {}'.format(type(response.text)))
+    print('\t- content: {}'.format(response.text))
