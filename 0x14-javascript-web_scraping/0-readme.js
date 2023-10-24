aa2a9f51bea3a2057af9a547ebcf15f6337b7839
njdam <jeandamngaboyimana@gmail.com>
2023-10-24 18:33:21 +0000

    A script that reads and prints the content of a file.

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,16 +5,16 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 // To check if file path is exists or not
-if (!filePath){
-	console.log('Usage: ./read_file.js <file_path>');
-	process.exit(1);
+if (!filePath) {
+  console.log('Usage: ./read_file.js <file_path>');
+  process.exit(1);
 }
 
 // Read file to get and print error or data
 fs.readFile(filePath, 'utf-8', (err, data) => {
-	if (err) {
-		console.error(err);
-	} else {
-		console.log(data);
-	}
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
 });
