bf8832c4111c6abe1723af18ccbe7eda38216782
njdam <jeandamngaboyimana@gmail.com>
2023-10-25 19:34:05 +0000

    7. Who was playing in this movie?

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -3,14 +3,35 @@
 
 const request = require('request');
 const movieId = process.argv[2];
-const apiUrl = 'https://swapi-api.alx-tools.com/api'
 
-data_StarWar = request(f'{apiUrl}/people/{movieId}')
+const movieURL = `https://swapi.dev/api/films/${movieId}/`;
 
-request(apiUrl, (error, response, body) => {
-	if (!error && response.statusCode === 200) {
-		console.log(body)
-	} else {
-		console.error('Error:', error);
-	}
+request(movieURL, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    process.exit(1);
+  }
+
+  if (response.statusCode === 200) {
+    const movieData = JSON.parse(body);
+    const characters = movieData.characters;
+
+    if (characters.length === 0) {
+      console.log(`No characters found for movieId ${movieId}`);
+    } else {
+      characters.forEach((characterURL, index) => {
+        request(characterURL, (charError, charResponse, charBody) => {
+          if (charError) {
+            console.error(`Error in fetching character ${index + 1}:`, charError);
+          } else if (charResponse.statusCode === 200) {
+            const characterData = JSON.parse(charBody);
+            console.log(characterData.name);
+          }
+        });
+      });
+    }
+  } else {
+    console.error('Error: Movie not found.');
+    process.exit(1);
+  }
 });
