449d0685922bb03c6e7384e564c95bc800609ee9
njdam <jeandamngaboyimana@gmail.com>
2023-11-01 15:14:00 +0000

    2. Click and turn red

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,8 +1,8 @@
 // Strict mode to catch common coding errors and to improve security
 'use strict';
 // A script to change header text color to red when clicked on
-$(document).ready(function() {
-  $('DIV#red_header').click(function() {
-    $('header').css('color', '#FF0000')
-  })
+$(document).ready(function () {
+  $('DIV#red_header').click(function () {
+    $('header').css('color', '#FF0000');
+  });
 });
