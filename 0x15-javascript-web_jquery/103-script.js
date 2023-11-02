fcf27383b4adb35554de23b90429f1861b8028ee
njdam <jeandamngaboyimana@gmail.com>
2023-11-02 00:02:22 +0000

    13. And press ENTER.

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -2,18 +2,27 @@
 'use strict';
 /*
 Fetches and prints how to say “Hello” depending on the language
-You should use this API service: https://www.fourtonfish.com/hellosalut/hello/
+API service: https://www.fourtonfish.com/hellosalut/hello/
 The language code will be the value entered in the tag INPUT#language_code (ex: es, fr, en etc.)
-The translation must be fetched when the user clicks on INPUT#btn_translate
+The translation must be fetched when the user clicks on INPUT#btn_translate or
+presses ENTER when the focus is on INPUT#language_code
 The translation of “Hello” must be displayed in the HTML tag DIV#hello
 */
 $(document).ready(function () {
-  $('INPUT#btn_translate').click(function () {
+  function fetchTranslation () {
     const languageCode = $('INPUT#language_code').val();
     const apiUrl = `https://www.fourtonfish.com/hellosalut/hello/?lang=${languageCode}`;
 
     $.get(apiUrl, function (data) {
       $('DIV#hello').html(data.hello);
     });
+  }
+
+  $('INPUT#btn_translate').click(fetchTranslation);
+
+  $('INPUT#language_code').keypress(function (event) {
+    if (event.key === 'Enter') {
+      fetchTranslation();
+    }
   });
 });
